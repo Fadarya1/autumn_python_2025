9ef30803868d7299af1a97c3791939a9c934e9c6
Fadeeva_Daria <fadarya1@yandex.ru>
2025-10-21 16:38:31 +0300

    initial commit

--- Task31.py
+++ Task31.py
@@ -9,11 +9,20 @@ log_entries = [
     "192.168.1.5 - POST /submit 403 1.5s"
 ]
 
+for line in log_entries:
+    line = line.split(" ")
+    if (line[4][0] == "4") or (line[4][0] == "5"):
+        print(line[0])
+
+
+
+
+
 #Результат: ['192.168.1.2', '192.168.1.3', '192.168.1.5']
 
-ip_200 = [line for line in log_entries if "200" in line]
-all_entries_with_errors = [item for item in log_entries if item not in ip_200]
-
-for ip in all_entries_with_errors:
-    show_ip = ip.split(" - ")
-    print(show_ip[0])
+# ip_200 = [line for line in log_entries if "200" in line]
+# all_entries_with_errors = [item for item in log_entries if item not in ip_200]
+#
+# for ip in all_entries_with_errors:
+#     show_ip = ip.split(" - ")
+#     print(show_ip[0])
